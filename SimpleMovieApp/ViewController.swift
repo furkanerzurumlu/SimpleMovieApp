8f5a5b74695788811a30680caa3895c27c2d718c
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2021-12-10 20:55:30 +0300

    Initial Commit

---
+++ SimpleMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMovieApp
+//
+//  Created by Furkan Erzurumlu on 10.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
